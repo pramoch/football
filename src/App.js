2606456fe6b4bf1f30f2c6774564f8934871475d
Pramoch Chulapong <pramoch.chulapong@gmail.com>
2020-10-21 14:34:44 +0700

    Adjust app's height.

--- src/App.js
+++ src/App.js
@@ -4,14 +4,18 @@ import {
   Switch,
   Route
 } from "react-router-dom";
+import NavBar from './components/NavBar/NavBar';
 import Home from './components/Home/Home';
 import Fixtures from './components/Fixtures/Fixtures';
 import Table from './components/Table/Table';
+import styles from './App.module.css';
 
 function App() {
   return (
     <Router>
-      <Switch>
+      <div className={styles.app}>
+        <NavBar />
+        <Switch>
           <Route path="/table">
             <Table />
           </Route>
@@ -22,6 +26,7 @@ function App() {
             <Home />
           </Route>
         </Switch>
+      </div>
     </Router>
   );
 }
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NarBar = () => {
+  return (
+    <div>NarBar</div>
+  )
+}
+
+export default NarBar;
