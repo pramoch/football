a58bd955a90acbdbd32e0b685887aff38a88a1e3
Pramoch Chulapong <pramoch.chulapong@gmail.com>
2020-10-21 14:23:28 +0700

    Add router. Add Home, Table, Fixtures components.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
 import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Home from './components/Home/Home';
+import Fixtures from './components/Fixtures/Fixtures';
+import Table from './components/Table/Table';
 
 function App() {
   return (
-    <div className="App">
-      App
-    </div>
+    <Router>
+      <Switch>
+          <Route path="/table">
+            <Table />
+          </Route>
+          <Route path="/fixtures">
+            <Fixtures />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+    </Router>
   );
 }
+
+// function Home() {
+//   return <h2>Home</h2>;
+// }
+
+// function Table() {
+//   return <h2>Table</h2>;
+// }
+
+// function Fixtures() {
+//   return <h2>Fixtures</h2>;
+// }
 
 export default App;
