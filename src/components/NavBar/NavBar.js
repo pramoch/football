3ba22b3c2fd404191efa683e86f4d39b19909807
Pramoch Chulapong <pramoch.chulapong@gmail.com>
2020-10-21 14:39:30 +0700

    Add Link in NavBar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NarBar = () => {
   return (
-    <div>NarBar</div>
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/table">Table</Link>
+      </li>
+      <li>
+        <Link to="/fixtures">Fixtures</Link>
+      </li>
+    </ul>
   )
 }
 
