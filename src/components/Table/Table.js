8cf3fa1feb329cc95dfe6e91de0a0d4261c7fbeb
Pramoch Chulapong <pramoch.chulapong@gmail.com>
2020-10-22 15:55:08 +0700

    Table component: Dynamic render

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -2,6 +2,250 @@ import React from 'react';
 import styles from './Table.module.css';
 
 const Table = () => {
+
+  const data = [
+    {
+      position: 1,
+      club: 'Everton',
+      played: 5,
+      won: 4,
+      drawn: 1,
+      lost: 0,
+      gf: 14,
+      ga: 7,
+      gd: '+7',
+      points: 13
+    },
+    {
+      position: 2,
+      club: 'Aston Villa',
+      played: 4,
+      won: 4,
+      drawn: 0,
+      lost: 0,
+      gf: 12,
+      ga: 2,
+      gd: '+10',
+      points: 12
+    },
+    {
+      position: 3,
+      club: 'Liverpool',
+      played: 5,
+      won: 3,
+      drawn: 1,
+      lost: 1,
+      gf: 13,
+      ga: 13,
+      gd: '0',
+      points: 12
+    },
+    {
+      position: 4,
+      club: 'Leicester City',
+      played: 5,
+      won: 3,
+      drawn: 0,
+      lost: 2,
+      gf: 12,
+      ga: 8,
+      gd: '+4',
+      points: 9
+    },
+    {
+      position: 5,
+      club: 'Arsenal',
+      played: 5,
+      won: 3,
+      drawn: 0,
+      lost: 2,
+      gf: 8,
+      ga: 6,
+      gd: '+2',
+      points: 9
+    },
+    {
+      position: 6,
+      club: 'Wolvehampton Wanderers',
+      played: 5,
+      won: 3,
+      drawn: 0,
+      lost: 2,
+      gf: 5,
+      ga: 7,
+      gd: '-2',
+      points: 9
+    },
+    {
+      position: 7,
+      club: 'Tottenham Hotspur',
+      played: 5,
+      won: 2,
+      drawn: 2,
+      lost: 1,
+      gf: 15,
+      ga: 8,
+      gd: '+7',
+      points: 8
+    },
+    {
+      position: 8,
+      club: 'Chelsea',
+      played: 5,
+      won: 2,
+      drawn: 2,
+      lost: 1,
+      gf: 13,
+      ga: 9,
+      gd: '+4',
+      points: 8
+    },
+    {
+      position: 9,
+      club: 'West Ham United',
+      played: 5,
+      won: 2,
+      drawn: 1,
+      lost: 2,
+      gf: 11,
+      ga: 7,
+      gd: '+4',
+      points: 7
+    },
+    {
+      position: 10,
+      club: 'Leeds United',
+      played: 5,
+      won: 2,
+      drawn: 1,
+      lost: 2,
+      gf: 9,
+      ga: 9,
+      gd: '0',
+      points: 7
+    },
+    {
+      position: 11,
+      club: 'Manchester City',
+      played: 4,
+      won: 2,
+      drawn: 1,
+      lost: 1,
+      gf: 7,
+      ga: 7,
+      gd: '0',
+      points: 7
+    },
+    {
+      position: 12,
+      club: 'Southampton',
+      played: 5,
+      won: 2,
+      drawn: 1,
+      lost: 2,
+      gf: 8,
+      ga: 9,
+      gd: '-1',
+      points: 7
+    },
+    {
+      position: 13,
+      club: 'Newcastle United',
+      played: 5,
+      won: 2,
+      drawn: 1,
+      lost: 2,
+      gf: 7,
+      ga: 9,
+      gd: '-2',
+      points: 7
+    },
+    {
+      position: 14,
+      club: 'Crystal Palace',
+      played: 5,
+      won: 2,
+      drawn: 1,
+      lost: 2,
+      gf: 6,
+      ga: 8,
+      gd: '-2',
+      points: 7
+    },
+    {
+      position: 15,
+      club: 'Manchester United',
+      played: 4,
+      won: 2,
+      drawn: 0,
+      lost: 2,
+      gf: 9,
+      ga: 12,
+      gd: '-3',
+      points: 6
+    },
+    {
+      position: 16,
+      club: 'Brighton and Hove Albion',
+      played: 5,
+      won: 1,
+      drawn: 1,
+      lost: 3,
+      gf: 9,
+      ga: 11,
+      gd: '-2',
+      points: 4
+    },
+    {
+      position: 17,
+      club: 'West Bromwich Albion',
+      played: 5,
+      won: 0,
+      drawn: 2,
+      lost: 3,
+      gf: 5,
+      ga: 13,
+      gd: '-8',
+      points: 2
+    },
+    {
+      position: 18,
+      club: 'Burnley',
+      played: 4,
+      won: 0,
+      drawn: 1,
+      lost: 3,
+      gf: 3,
+      ga: 8,
+      gd: '-5',
+      points: 1
+    },
+    {
+      position: 19,
+      club: 'Sheffield United',
+      played: 5,
+      won: 0,
+      drawn: 1,
+      lost: 4,
+      gf: 2,
+      ga: 7,
+      gd: '-5',
+      points: 1
+    },
+    {
+      position: 20,
+      club: 'Fulham',
+      played: 5,
+      won: 0,
+      drawn: 1,
+      lost: 4,
+      gf: 4,
+      ga: 12,
+      gd: '-8',
+      points: 1
+    }
+  ]
+
   return (
     <div className={styles.wrapper}>
       <table>
@@ -20,18 +264,24 @@ const Table = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>Everton</td>
-            <td>5</td>
-            <td>4</td>
-            <td>1</td>
-            <td>0</td>
-            <td>14</td>
-            <td>7</td>
-            <td>7</td>
-            <td>13</td>
-          </tr>
+          {
+            data.map((team, index) => {
+              return (
+                <tr>
+                  <td>{team.position}</td>
+                  <td>{team.club}</td>
+                  <td>{team.played}</td>
+                  <td>{team.won}</td>
+                  <td>{team.drawn}</td>
+                  <td>{team.lost}</td>
+                  <td>{team.gf}</td>
+                  <td>{team.ga}</td>
+                  <td>{team.gd}</td>
+                  <td>{team.points}</td>
+                </tr>
+              )
+            })
+          }
         </tbody>
       </table>
     </div>
